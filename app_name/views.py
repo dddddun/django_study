4f114c98e4088a2b92c2ec72d7072f3c0b2b38f0
dddddun <leaf1993@naver.com>
2021-08-26 11:58:14 +0900

    Feat: Add reply function

--- app_name/views.py
+++ app_name/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Question
 from django.core.paginator import Paginator
+from .forms import QuestionForm, AnswerForm
+
 
 
 # 질문 목록 보기
@@ -26,12 +28,37 @@ def detail(request, question_id):
     context = {'question': question}
     return render(request, 'app_name/question_detail.html', context)
 
+
+# 질문글 등록
+def question_create(request):
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.create_date = timezone.now()
+            question.save()
+            return redirect('app_name:index')
+    else:
+        form = QuestionForm()
+    context = {'form': form}
+    return render(request, 'app_name/question_form.html', context)
+
+
 # 질문에 대한 답변 생성하기 1
 def answer_create(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
-    # 답변 생성 후 질문 상세 화면을 다시 보여주기 위한 redirect함수 사용
-    return redirect('app_name:detail', question_id=question_id)
+    if request.method == "POST":
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.create_date = timezone.now()
+            answer.question = question
+            answer.save()
+            return redirect('app_name:detail', question_id=question.id)
+    else:
+        form = AnswerForm()
+    context = {'question': question, 'form': form}
+    return render(request, 'app_name/question_detail.html', context)
 
 # 질문에 대한 답변 생성하기 2 (Answer 모델을 import해서 직접 사용하는 방법)
 # def answer_create(request, question_id):
