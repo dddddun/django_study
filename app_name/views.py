359ffa1b970151d12680f0da2a746dc0d5a2f96d
dddddun <leaf1993@naver.com>
2021-08-30 16:18:12 +0900

    Feat: Add various functions

--- app_name/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from django.utils import timezone
-from .models import Question, Answer, Comment
-from django.core.paginator import Paginator
-from .forms import QuestionForm, AnswerForm, CommentForm
-from django.contrib.auth.decorators import login_required
-from django.contrib import messages
