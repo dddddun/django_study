647e08e985a733b17cb342592d5603b4fac0c1c9
dddddun <leaf1993@naver.com>
2021-08-31 10:26:27 +0900

    Fefactor: urls refactoring

--- app_name/models.py
+++ app_name/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 # Question 모델의 속성 = 제목(subject), 내용(content), 작성일시(create_date)
 class Question(models.Model):
     # 작성자
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_question')
     # 글자수의 길이가 제한된 텍스트 = CharField 메서드 사용
     subject = models.CharField(max_length=200)
     # 글자수의 길이가 제한되지 않은 텍스트 = TextField 메서드 사용
@@ -14,6 +14,8 @@ class Question(models.Model):
     create_date = models.DateTimeField()
     # 수정일시 (수정한 경우에만 생성되므로 null, blank 값이 True)
     modify_date = models.DateTimeField(null=True, blank=True)
+    # 추천인 속성
+    voter = models.ManyToManyField(User, related_name='voter_question')
 
     # 아이디값 대신 제목을 표시하기
     def __str__(self):
@@ -22,7 +24,7 @@ class Question(models.Model):
 
 class Answer(models.Model):
     # 작성자
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_answer')
 
     # 질문에 대한 답변에 관한 모델이므로 Question 모델을 속성으로 가져가야 한다.
     # 기존 모델을 속성으로 연결하려면 ForeignKey를 사용해야 한다.
@@ -32,6 +34,7 @@ class Answer(models.Model):
     content = models.TextField()
     create_date = models.DateTimeField()
     modify_date = models.DateTimeField(null=True, blank=True)
+    voter = models.ManyToManyField(User, related_name='voter_answer')
 
 
 class Comment(models.Model):
--- config/urls.py
+++ config/urls.py
@@ -1,21 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
-from app_name import views
+from app_name.views import base_views
 # url 앱 별로 분류하기
 # from app_name import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-
-    # app_name/ URL이 요청되면 view.index를 호출하라는 매핑
-    # view.index는 views.py 파일의 index 함수를 의미한다.
-    # path('app_name/', views.index),
-
-    # url 앱 별로 분류하기
-    # app_name/ 으로 시작하는 페이지를 요청하면 app_name/urls.py 파일의 매핑 정보를 읽어서 처리해라.
-
     path('app_name/', include('app_name.urls')),
+    path('admin/', admin.site.urls),
     path('common/', include('common.urls')),
     # '/'에 해당되는 path
-    path('', views.index, name='index'),
+    path('', base_views.index, name='index'),
 ]
